f13e9a71c9ae29a5eb9a746484213c33ea0f505c
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-01-14 21:18:26 +0100

    Passing data from recipe-list to recipe-item.

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("Test Recipe", "Description test", "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale"),
+    new Recipe("Test Recipe", "Description test test", "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale"),
     new Recipe("Another recipe", "More descriptions", "https://thekitchencommunity.org/wp-content/uploads/2021/02/43-Foods-That-Start-With-E-1200x675.jpg"),
   ];
 
