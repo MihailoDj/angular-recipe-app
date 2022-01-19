1690a37f11d7817fb8ff5c1f1fd2b20fb670368a
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-01-19 23:08:33 +0100

    Expanding Recipe model with an Ingredient list.

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../components/recipes/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,20 @@ import { Recipe } from '../components/recipes/recipe.model';
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe("Test Recipe", "Description test test", "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale"),
-    new Recipe("Another recipe", "More descriptions", "https://thekitchencommunity.org/wp-content/uploads/2021/02/43-Foods-That-Start-With-E-1200x675.jpg"),
+    new Recipe("Test Recipe", 
+    "Description test test", 
+    "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    [
+      new Ingredient("Meat", 1),
+      new Ingredient("French fries", 20)
+    ]),
+    new Recipe("Another recipe", 
+    "More descriptions", 
+    "https://thekitchencommunity.org/wp-content/uploads/2021/02/43-Foods-That-Start-With-E-1200x675.jpg",
+    [
+      new Ingredient("Bread", 2),
+      new Ingredient("Meat", 1)
+    ]),
   ];
 
   constructor() { }
