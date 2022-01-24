a6e1120857388a60c2d926564c3d040d53e5e314
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-01-24 18:58:04 +0100

    Adding routing for edit and add new recipe.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailsComponent } from './components/recipes/recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
     {path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipeStartComponent},
-        {path: ':id', component: RecipeDetailsComponent}
+        {path: 'new', component: RecipeEditComponent},
+        {path: ':id', component: RecipeDetailsComponent},
+        {path: ':id/edit', component: RecipeEditComponent},
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListService } from './services/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-s
     ShoppingListEditComponent,
     DropdownDirective,
     RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [
     BrowserModule,
