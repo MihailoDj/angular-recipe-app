87f3d34561d7f84d6cca44c91e9bcb775affe8a6
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-01-25 20:27:47 +0100

    Replacing event emitters with subjects.

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe.service';
-import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -9,16 +8,10 @@ import { Recipe } from './recipe.model';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe | undefined;
-
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.selectedRecipe.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
+    
   }
 
 }
