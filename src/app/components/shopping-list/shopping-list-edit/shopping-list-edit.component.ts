e8f2944d8a982feff4f0bbf91fe52f7ee8307cac
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-01-26 22:00:46 +0100

    Making the shopping-list form submittable.

--- src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -8,19 +9,15 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false})
-  nameInputRef!: ElementRef;
-  @ViewChild('amountInput', {static: false})
-  amountInputRef!: ElementRef;
-
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    this.shoppingListService.addIngredient(
-      new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value));
+  onAddItem(form: NgForm) {
+    const value = form.value;
+
+    this.shoppingListService.addIngredient(new Ingredient(value.name, value.amount));
   }
 
 }
