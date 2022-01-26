44e3765925e780e0c01709048d9d6afd8bf8c758
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-01-26 22:16:26 +0100

    Loading selected ingredient info into SL form.

--- src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -8,16 +9,37 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', {static: false}) slForm!: NgForm;
+  subscription!: Subscription;
+  editMode: boolean = false;
+  edittedItemIndex!: number;
+  edittedItem!: Ingredient;
+
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.edittedItemIndex = index;
+        this.edittedItem = this.shoppingListService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.edittedItem.name,
+          amount: this.edittedItem.amount
+        });
+      }
+    );
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
 
     this.shoppingListService.addIngredient(new Ingredient(value.name, value.amount));
+  }
+
+  ngOnDestroy(): void {
+      this.shoppingListService.startedEditing.unsubscribe();
   }
 
 }
